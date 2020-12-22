e586954e16e4b7bff3afb52ef690f69391cc7ef4
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2020-12-22 10:05:07 +0200

    Add Keys.swift

---
+++ Marvel/Supporting files/Keys.swift
@@ -0,0 +1,14 @@
+//
+//  Keys.swift
+//  Marvel
+//
+//  Created by Olena Stepaniuk on 14.12.2020.
+//
+
+import Foundation
+
+struct Keys {
+    static let PUBLIC_KEY = ""
+    static let PRIVATE_KEY = ""
+    static let HASH = ""
+}
